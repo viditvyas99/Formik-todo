cca59f5e29e7f6e687d8de50b8307553ced33cf7
Vidit Vyas <viditvyas98@gmail.com>
2024-06-26 18:11:45 +0530

    have to complete

--- projects/login-todo/src/component/Todos.jsx
+++ projects/login-todo/src/component/Todos.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useSelector ,useDispatch} from 'react-redux'
-import { addTodo, removeTodo } from '../features/todo/todoSlice'
+import { addTodo, moveTodo, removeTodo, reoderTodos } from '../features/todo/todoSlice'
 
 
 
@@ -28,13 +28,21 @@ export default function Todos() {
     }
     const handleOnDragEnd =(result)=>{
         if(!result.destination) return
-        const {souce,destination,dragableId}=result
-        if(souce.droppableId===destination.droppableId){
-            
+        const {source,destination,dragableId}=result
+        if(source.droppableId===destination.droppableId){
+            const reorderItems=Array.from(todos[source.droppableId])
+            const [movedItem]=reorderItems.splice(source.index,1)
+            reorderItems.splice(destination.index,0,movedItem)
+            dispatch(reoderTodos({column:source.droppableId,reorderItems}))
+        }
+        else{
+            const movedItem=todos[source.droppableId].find(item=>item.id===dragableId)
+            dispatch(moveTodo({
+                source:source.droppableId,
+                item:movedItem
+            }))
         }
     }
-
-
   return (
     <div>
     
